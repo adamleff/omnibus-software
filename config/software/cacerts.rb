0684ea43d0af891334cb3ed8ecb311d1be078566
Adam Leff <aleff@chef.io>
2015-09-10 17:10:00 -0400

    Removing the read of the CA certificate bundle before appending the Verisign 1024-bit certs.
    Chef is supported on platforms that do not support a UTF-8 locale (such as Wind River Linux)
    and the read of the file to check if the Verisign certs are already there causes Omnibus
    to unwind.  Since OpenSSL doesn't care if a cert is in a bundle twice, I'm removing the
    conditional check before appending the certs and just blindly append.

--- config/software/cacerts.rb
+++ config/software/cacerts.rb
@@ -60,9 +60,7 @@ build do
 
   # Append the 1024bit Verisign certs so that S3 continues to work
   block do
-    unless File.foreach("#{project_dir}/cacert.pem").grep(/^Verisign Class 3 Public Primary Certification Authority$/).any?
-      File.open("#{project_dir}/cacert.pem", "a") { |fd| fd.write(VERISIGN_CERTS) }
-    end
+    File.open("#{project_dir}/cacert.pem", "a") { |fd| fd.write(VERISIGN_CERTS) }
   end
 
   copy "#{project_dir}/cacert.pem", "#{install_dir}/embedded/ssl/certs/cacert.pem"
